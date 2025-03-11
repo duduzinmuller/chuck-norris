1c7b0f6a0d32012f709339b2097c4e5d4684c010
Eduardo <eduardomuller989@gmail.com>
2025-03-11 15:14:56 -0400

    chore: remove babel setup

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [
-    "@babel/preset-env", // Transforma código ES6+ para código compatível com versões mais antigas do Node.js.
-    "@babel/preset-react", // Suporte para JSX.
-    "@babel/preset-typescript", // Suporte para TypeScript.
-  ],
-};
