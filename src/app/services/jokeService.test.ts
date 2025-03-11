c6ac30f6ac0db60dae7a490b62f0504763a8817d
Eduardo <eduardomuller989@gmail.com>
2025-03-10 23:03:32 -0400

    test: ensure return categories successfully

--- src/app/services/jokeService.test.ts
+++ src/app/services/jokeService.test.ts
@@ -5,19 +5,26 @@ import {
 } from "./jokeService";
 import axios from "axios";
 
+// Mock axios
 jest.mock("axios");
 
 describe("jokeService functions", () => {
+  // Test for fetchRandomJoke
   describe("fetchRandomJoke", () => {
-    it("should return categories successfully", async () => {
+    it("should return a random joke successfully", async () => {
+      // Mock API response
       const mockResponse = {
-        data: ["animal", "celebrity", "dev"],
+        data: {
+          icon_url: "https://example.com/icon.png",
+          id: "123",
+          value: "Chuck Norris can divide by zero.",
+        },
       };
 
       (axios.get as jest.Mock).mockResolvedValue(mockResponse); // Mocking axios
 
-      const categories = await fetchCategories();
-      expect(categories).toEqual(mockResponse.data);
+      const joke = await fetchRandomJoke();
+      expect(joke).toEqual(mockResponse.data); // Check if the returned response matches the mock
     });
 
     it("should throw an error if the API fails", async () => {
@@ -27,8 +34,23 @@ describe("jokeService functions", () => {
       try {
         await fetchRandomJoke();
       } catch (error) {
-        expect(error).toEqual(new Error("Could not fetch the joke.")); // Esperando a mensagem de erro em inglês
+        expect(error).toEqual(new Error("Could not fetch the joke.")); // Check if the error is as expected
       }
+    });
+  });
+
+  // Test for fetchCategories
+  describe("fetchCategories", () => {
+    it("should return categories successfully", async () => {
+      // Mock API response
+      const mockResponse = {
+        data: ["animal", "celebrity", "dev"],
+      };
+
+      (axios.get as jest.Mock).mockResolvedValue(mockResponse); // Mocking axios
+
+      const categories = await fetchCategories();
+      expect(categories).toEqual(mockResponse.data); // Check if the returned categories match the mock
     });
   });
 });
