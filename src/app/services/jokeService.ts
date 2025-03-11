7067fb368213ba8a66e93407356ba8ba216fe496
Eduardo <eduardomuller989@gmail.com>
2025-03-10 22:40:24 -0400

    feat: update jokeService.ts with new API endpoints and types

--- src/app/services/jokeService.ts
+++ src/app/services/jokeService.ts
@@ -1,12 +1,21 @@
 import axios from "axios";
 import { Joke } from "../types/type";
 
+/**
+ * Representa a resposta da API para uma piada aleatória.
+ */
 interface JokeResponse {
   icon_url: string;
   id: string;
   value: string;
 }
 
+/**
+ * Busca uma piada aleatória da API do Chuck Norris.
+ *
+ * @returns {Promise<JokeResponse>} Uma promessa que resolve com os dados da piada.
+ * @throws {Error} Lança um erro se não for possível buscar a piada.
+ */
 export const fetchRandomJoke = async (): Promise<JokeResponse> => {
   try {
     const response = await axios.get("https://api.chucknorris.io/jokes/random");
@@ -17,11 +26,15 @@ export const fetchRandomJoke = async (): Promise<JokeResponse> => {
   }
 };
 
+/**
+ * Busca as categorias disponíveis de piadas da API do Chuck Norris.
+ *
+ * @returns {Promise<string[]>} Uma promessa que resolve com um array de strings representando as categorias.
+ * @throws {Error} Lança um erro se não for possível carregar as categorias.
+ */
 export const fetchCategories = async (): Promise<string[]> => {
   try {
-    const response = await axios.get(
-      "https://api.chucknorris.io/jokes/categories",
-    );
+    const response = await axios.get("https://api.chucknorris.io/jokes/categories");
     return response.data;
   } catch (error) {
     console.error("Erro ao buscar categorias:", error);
@@ -29,13 +42,19 @@ export const fetchCategories = async (): Promise<string[]> => {
   }
 };
 
+/**
+ * Busca uma piada da API do Chuck Norris com base em uma categoria específica.
+ *
+ * @param {string} category - A categoria para a qual deseja buscar uma piada.
+ * @returns {Promise<Joke>} Uma promessa que resolve com um objeto do tipo Joke contendo os dados da piada.
+ * @throws {Error} Lança um erro se não for possível buscar a piada para a categoria informada.
+ */
 export const fetchJokeByCategory = async (category: string): Promise<Joke> => {
   try {
     const response = await axios.get(
       `https://api.chucknorris.io/jokes/random?category=${category}`
     );
 
-    // Retornando um objeto Joke completo
     return {
       icon_url: response.data.icon_url,
       id: response.data.id,
